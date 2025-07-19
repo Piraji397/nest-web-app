cec8ce02148924f9ab4caf14109fa65f3f4b985a
Piraji397 <pirajitrimukhe397@gmail.com>
2025-07-19 13:28:38 +0530

    created user dto and added validation as well

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    name:string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email:string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(4)
+    password:string;
+}
